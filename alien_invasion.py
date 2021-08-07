5ac25996f5f466285e44223b426fc06a3843d8fe
laura <evilauraaa@gmail.com>
2021-08-07 20:37:18 +0800

    Initial Commit

---
+++ alien_invasion.py
@@ -0,0 +1,27 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasioin")
+        
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                    
+            pygame.display.flip()
+            
+if __name__=="__main__":
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
