6c60fd669052e22fe18ce1cf9c5f9f946fe40d26
laura <evilauraaa@gmail.com>
2021-08-14 15:37:55 +0800

    Moving da ship left and right.

--- ship.py
+++ ship.py
@@ -6,7 +6,12 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
+        
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
         
         # Load the ship image and get its rect.
         self.image = pygame.image.load("images/ship.bmp")
@@ -14,6 +19,18 @@ class Ship:
         
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+        
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+        
+    def update(self):
+        if self.moving_right:
+            self.x += self.settings.ship_speed
+            
+        if self.moving_left:
+            self.rect.x -= self.settings.ship_speed
+        
+        self.rect.x = self.x
         
     def blitme(self):
         """Draw the ship at its current location."""
