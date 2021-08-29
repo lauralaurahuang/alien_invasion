2dbbaa74b749161e3ed9a3fbe58354a505d95e2d
laura <evilauraaa@gmail.com>
2021-08-29 17:22:01 +0800

    Dancing aliens!

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,6 +31,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             
             
@@ -78,6 +79,15 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+                
+    def _update_aliens(self):
+        """
+        Check if the fleet in at an edge, 
+        then update the positions of all aliens in the fleet.
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
+        
             
     def _create_fleet(self):
         """Create the fleet of aliens."""
@@ -99,8 +109,19 @@ class AlienInvasion:
         for row_number in range(number_rows):
             for alien_number in range(number_aliens_x):
                 self._create_alien(alien_number, row_number)
-
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
             
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+                    
     def _create_alien(self, alien_number, row_number): 
         """"Create an alien and place it in the row."""   
         alien = Alien(self)
--- settings.py
+++ settings.py
@@ -14,3 +14,7 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # Fleet_direction of 1 represent right, -1 represent left.
+        self.fleet_direction = 1
