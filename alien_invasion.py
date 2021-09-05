bc31a75e19280df57e8555980f21a324e4382a1d
laura <evilauraaa@gmail.com>
2021-09-05 19:32:07 +0800

    I can shoot!

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,6 +79,19 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        
+        self._check_bullet_alien_collisions()
+        
+    def _check_bullet_alien_collisions(self):
+        
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
+        
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
                 
     def _update_aliens(self):
         """
